0f8f46d631099e5bba7e970ee6a145d7082ce38b
SDJoeKing <linghao.zhou@outlook.com>
2025-03-25 20:14:21 +0000

    Update testFile.cpp

--- testFile.cpp
+++ testFile.cpp
@@ -159,56 +159,7 @@ int main(int args, char** argc)
         std::cout << thickCal(data, vel, fs, id, targetThickness, _thres) << std::endl;
          
     }
- 
-
 
     return 0;
 }
 
-
- 
-
-// // @brief: Function calculating the thickness based on inputs of velocity, sampling rate, pipe inner diameter, norminal thickness and threshold to try in (0-1]
-// float thickCal(const float *arr,  float velocity, float fs, float id, float targetThick, float _thres)
-// {
-
-//     // 0. preparation calculation
-//     float maxValue = argmax(arr, dataLength / 2); // we know for certain the peak value is within the first half.
-//     float threshold = _thres * maxValue;
-
-//     uint16_t waterGap = id/1000 / vel_water * fs;
-//     uint16_t targetThickDataPoint = targetThick/1000 * 2 / velocity * fs;
-
-
-//     // 1. front wall with waterGap data points margin in the beginning
-  
-//     uint16_t gateA_f = argFirstLarger(arr, waterGap , dataLength, threshold);
-//     std::cout << "threshold "<< threshold << std::endl;
-//     if(gateA_f == 0)
-//         return 0.1;
-
-//     uint16_t gateA_b = argFirstSmaller(arr,gateA_f, gateA_f + targetThickDataPoint,  threshold); // try to locate the falling edge within 1 time the nominal thickness range
-//     if(gateA_b == 0)
-//         return 0.2;
-
-//     uint16_t gateA = (gateA_f + gateA_b) / 2;
-
-
-//     // 2. find backwall
-//     uint16_t margin = gateA_b + (pulseWidth*2/1000 / velocity * fs); // margin of pulse, i.e. minimum resolution of scan
-//     uint16_t gateB_f = argFirstLarger(arr, margin, margin + targetThickDataPoint, threshold);
-//     if(gateB_f == 0)
-//         return 0.3;
-
-//     uint16_t gateB_b = argFirstSmaller(arr,gateB_f, margin + targetThickDataPoint, threshold);
-//     if(gateB_b == 0)
-//         return 0.4;
-
-//     uint16_t gateB = (gateB_f + gateB_b)/2;
-    
-//     // 3. return calculated thickness
-//     std::cout << "GateA_f: " << gateA_f << " GateA_b: " << gateA_b << " GateB_f: " << gateB_f << " GateB_b: " << gateB_b << std::endl;
-//     return (gateB - gateA) / 2.0 / fs * velocity *1000;
-// }
-
- 
